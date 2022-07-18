446bb24a34a1237857265adff6d05bceab96ef07
kamila82 <99173975+kamila82@users.noreply.github.com>
2022-07-18 14:39:04 +0200

    Update Esercizio_JSON_2_input_multiplo.py

--- Esercizio_JSON_2_input_multiplo.py
+++ Esercizio_JSON_2_input_multiplo.py
@@ -1,33 +1,5 @@
 import json #una libreria
 
-# RICEVERE UN JSON
-x = '{"Nome": "Mirko", "Eta":"36", "Citta": "New York"}'
-
-y = json.loads(x) #carichiamo nell'elemento y i dati di x
-#print(y)
-#print(y["Eta"]) #una stringa che va stampata come un dizionario
-#print()
-
-# INVIARE e CREARE UN JSON
-x1 = {
-    "Nome": "Mirko", 
-    "Eta":"36", 
-    "Citta": "New York"
-    }
-
-y1 = json.dumps(x1) #carichiamo nell'elemento y i dati di x
-#print(y1) #un dizionario che va convertito in una stringa 
-
-#json.dumps(y1, indent=4)
-#json.dumps(y, indent=4, separators=(". ", ": "))
-#json.dumps(y, indent=4, sort_keys=True) #può ordinare automaticamente un elemento 
-#print(y1)
-
-# ESERCIZIO - creare una struttra di input che permetta di accedere a due sistemi:
-# 1) prenderà i dati in entrata e scriverà (dumps) un file di JSON salvando in una mio_elenco
-# 2) prenderà tra una mio_elenco un input e caricherà un file JSON
-# 3) il sistema dovrà aspettare l'END  per terminare e dovrà prima farti caricare almeno un dato 
-
 mio_elenco = {}
 x = 1
 
